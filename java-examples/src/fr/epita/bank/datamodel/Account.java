3daecb274b90f3d8509f567f0b1121ad2f416a40
thomasbroussard <thomasbroussard@users.noreply.github.com>
2026-06-22 15:06:30 +0200

    first integration

---
+++ java-examples/src/fr/epita/bank/datamodel/Account.java
@@ -0,0 +1,9 @@
+package fr.epita.bank.datamodel;
+
+
+public class Account {
+
+    Double balance;
+    Customer customer;
+
+}
---
+++ java-examples/src/fr/epita/bank/datamodel/StockTransaction.java
@@ -0,0 +1,15 @@
+package fr.epita.bank.datamodel;
+
+import java.time.LocalDate;
+
+public class StockTransaction {
+    Double unitPrice;
+    Integer quantity;
+    Double commission;
+    LocalDate txDate;
+
+    InvestmentAccount account;
+    Stock stock;
+
+
+}
