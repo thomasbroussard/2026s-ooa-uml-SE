517d34bbb35a8b8687a0cf42c26c903fbccf5e1f
tbr <tbr@tbr.com>
2026-09-15 10:47:28 +0200

    feat: save

--- biostats/test/fr/epita/biostats/test/JDBCTestFromDAO.java
+++ biostats/test/fr/epita/biostats/test/JDBCTestFromDAO.java
@@ -13,15 +13,30 @@ public class JDBCTestFromDAO {
 
         BioStatJDBCDAO dao = new BioStatJDBCDAO();
         try {
+            //save
             dao.save(new BiostatEntry("John", "M", 20, 170, 60));
             BiostatEntry bio = new BiostatEntry();
             bio.setName("Joh");
             bio.setGender("M");
+
+            //find
             List<BiostatEntry> entries = dao.find(bio);
             if (entries.size() != 1) {
                 System.out.println("Error size mismatch");
             }
             System.out.println(entries);
+            //update
+            entries.get(0).setAge(21);
+            dao.update(entries.get(0));
+
+            //check again
+            entries = dao.find(bio);
+
+            //delete
+            dao.delete(entries.get(0));
+
+            //check again
+            entries = dao.find(bio);
 
         } catch (DataAccessException e) {
             throw new RuntimeException(e);
